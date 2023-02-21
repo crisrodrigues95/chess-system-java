ef95a4aaf178bda7a9f4abc46e25eb16a9495d23
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-02-21 14:56:50 -0300

    First class position

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Programm {
 
 	public static void main(String[] args) {
 		
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
